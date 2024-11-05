5939c6bd497230acc8e2797923b808cec863af99
HersonAJ <hersonaguilar202230685@cunoc.edu.gt>
2024-11-04 19:24:00 -0600

    corriendo las rutas

--- Frontend/Proyecto2/src/app/app.routes.ts
+++ Frontend/Proyecto2/src/app/app.routes.ts
@@ -12,10 +12,11 @@ import { CargarEdicionComponent } from './cargar-edicion/cargar-edicion.componen
 import { VerArchivoComponent } from './ver-archivo/ver-archivo.component';
 import { RecargarCarteraComponent } from './recargar-cartera-editor/recargar-cartera-editor.component';
 import { CarteraGlobalComponent } from './cartera-global/cartera-global.component';
-//faltantes
+//la siguente ruta es la de la prueba
 import { EliminarRevistaComponent } from './eliminar-revista/eliminar-revista.component';
 import { RevistasSuscriptorComponent } from './revistas-suscriptor/revistas-suscriptor.component';
-
+import { MisSuscripcionesComponent } from './mis-suscripciones/mis-suscripciones.component';
+import { RevistaInfoComponent } from './revista-info/revista-info.component';
 
 
 
@@ -27,13 +28,16 @@ export const routes: Routes = [
   { path: 'perfil', component: PerfilComponent},
   { path: 'misPublicaciones', component: MisPublicacionesComponent},
   { path: 'crear-revista', component: CrearRevistaComponent},
-  { path: 'revista/:idRevista', component: RevistaComponent},
+  { path: 'revista/:idRevista', component: RevistaComponent}, 
   { path: 'cargar-edicion/:idRevista', component: CargarEdicionComponent},
   { path: 'ver-archivo/:idRevista/:idEdicion', component: VerArchivoComponent},
   { path: 'recargar-cartera', component: RecargarCarteraComponent},
   { path: 'recargar-cartera-global', component: CarteraGlobalComponent},
   { path: 'eliminar-revista', component: EliminarRevistaComponent},
-  { path: 'revistas', component: RevistasSuscriptorComponent}
+  { path: 'revistas', component: RevistasSuscriptorComponent},
+  { path: 'mis-suscripciones', component: MisSuscripcionesComponent},
+  { path: 'revista-info/:idRevista', component: RevistaInfoComponent }
+
 ];
 
 //agregue esto para tratar de hacer la referencia al registro
