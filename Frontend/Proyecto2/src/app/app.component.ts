974417d46b92c5f5ddb117aec1dd9c26534394bf
HersonAJ <hersonaguilar202230685@cunoc.edu.gt>
2024-11-07 20:28:51 -0600

    correcciones para mostrar los anuncios

--- Frontend/Proyecto2/src/app/app.component.ts
+++ Frontend/Proyecto2/src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { AnuncioComponent } from "./anuncio/anuncio.component";
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, RouterLink, RouterLinkActive], //falta agregar anuncios
+  imports: [HeaderComponent, RouterOutlet, RouterLink, RouterLinkActive, AnuncioComponent, CollapseModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
